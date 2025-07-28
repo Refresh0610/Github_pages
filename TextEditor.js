d0bc12985b00108b6a08b03ba30ae90fbf1d1faa
Refresh0610 <s22222@g.ashikaga.ac.jp>
2025-07-29 01:27:05 +0900

    Update TextEditor.js

--- TextEditor.js
+++ TextEditor.js
@@ -1,3 +1,11 @@
-import jquary
+ver container = '';
 
-ver container = ''
+function preview(container){
+  let x = "";
+  if(x != "/n"){
+    let x = $(".ql-editor").getHTML();
+    console.log(x);
+    let title = "";
+    title += "";
+  }
+}
