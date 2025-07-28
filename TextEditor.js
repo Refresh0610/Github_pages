1664b927e0327caf263b0d4dfa0d01ec9e369ccf
Refresh0610 <s22222@g.ashikaga.ac.jp>
2025-07-29 02:50:45 +0900

    Update TextEditor.js

--- TextEditor.js
+++ TextEditor.js
@@ -1,10 +1,10 @@
-ver container = '';
+var container = '';
 
 function preview(){
   let x = "";
   let html = $(".ql-editor").html(); // .getHTML() は存在しないので .html() を使う
   $('#preview-container').html(html);
-  Alert(html);
+  alert(html);
   /*if(x != "/n"){
     let title = "";
     title += "";
