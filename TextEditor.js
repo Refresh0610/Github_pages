7a4ca0796bb04bba5b6f7762cfe9d4e4a4f009a7
Refresh0610 <s22222@g.ashikaga.ac.jp>
2025-07-29 02:33:40 +0900

    Update TextEditor.js

--- TextEditor.js
+++ TextEditor.js
@@ -2,11 +2,11 @@ ver container = '';
 
 function preview(){
   let x = "";
-  if(x != "/n"){
-    let html = $(".ql-editor").html(); // .getHTML() は存在しないので .html() を使う
-    $('#preview-container').html(html);
-    console.log(x);
+  let html = $(".ql-editor").html(); // .getHTML() は存在しないので .html() を使う
+  $('#preview-container').html(html);
+  console.log(html);
+  /*if(x != "/n"){
     let title = "";
-    title += "";
+    title += "";*/
   }
 }
